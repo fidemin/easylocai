db3ef5748cd890b1a3d5d402843876926f1f4242
Yunhong Min <yunhongmin00@gmail.com>
2025-07-19 15:17:35 +0900

    mcp practice

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from mcp-client!")
+import asyncio
+import json
+from contextlib import AsyncExitStack
+
+from mcp import StdioServerParameters, stdio_client, ClientSession
+
+
+async def main():
+
+    with open("mcp_server_config.json") as f:
+        config = json.load(f)["mcpServers"]["filesystem"]
+
+    server_params = StdioServerParameters(
+        command=config["command"],
+        args=config["args"],
+        env=None
+    )
+
+    stack = AsyncExitStack()
+    async with stack:
+        stdio, write = await stack.enter_async_context(stdio_client(server_params))
+        session = await stack.enter_async_context(ClientSession(stdio, write))
+        await session.initialize()
+
+        tools_response = await session.list_tools()
+        tool_names = [tool.name for tool in tools_response.tools]
+
+        print(f"tools: {tool_names}")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
