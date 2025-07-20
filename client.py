0bbabcc1ae9911912c1e81885f67917e464b1e9a
Yunhong Min <yunhongmin00@gmail.com>
2025-07-20 19:51:07 +0900

    client auto select server and tool

--- client.py
+++ client.py
@@ -52,17 +52,22 @@ async def list_files(session, extensions: set[str]) -> list[str]:
     return matches
 
 
-MCP_TOOLS = {
-    # "list_allowed_directories": "Lists directories the user can browse.",
-    # "list_directory": "Lists files and directories at a given path. Requires: {'path': <str>}",
-    # "read_file": "Reads contents of a file. Requires: {'path': <str>}",
-}
+MCP_TOOLS = {}
 
 
-def choose_mcp_tool(client, user_query, user_context_list):
-    tool_description = "\n".join(
-        f"- {name}: {info}" for name, info in MCP_TOOLS.items()
-    )
+def choose_mcp_tool(client, user_query, user_context_list, mcp_tools_dict):
+    tool_description_lines = []
+    for server_name, tool_info_dict in mcp_tools_dict.items():
+        server_name = f"- Server Name: {server_name}"
+        tool_description_lines.append(server_name)
+
+        for tool_name, tool_info in tool_info_dict.items():
+            tool_name_text = f"    - Tool Name: {tool_name}"
+            tool_description_lines.append(tool_name_text)
+            tool_info_text = f"    - Tool Info: {tool_info}"
+            tool_description_lines.append(tool_info_text)
+
+    tool_description = "\n".join(tool_description_lines)
 
     user_contexts = "\n".join(
         [
@@ -75,6 +80,7 @@ def choose_mcp_tool(client, user_query, user_context_list):
     You are a tool-choosing assistant. A user wants to interact with a file system.
     You have access to the following MCP tools:
 
+    Tool Description:
     {tool_description}
 
     User query:
@@ -84,6 +90,7 @@ def choose_mcp_tool(client, user_query, user_context_list):
     {user_contexts}
 
     Return a JSON object with:
+    - server_name: one of the server name above
     - tool: one of the tool names above
     - args: any parameters needed (or empty if none)
 
@@ -110,41 +117,38 @@ def choose_mcp_tool(client, user_query, user_context_list):
 async def main():
     with open("mcp_server_config.json") as f:
         servers = json.load(f)["mcpServers"]
-        config = servers["filesystem"]
-        config1 = servers["custom"]
 
-    server_params = StdioServerParameters(
-        command=config["command"], args=config["args"], env=None
-    )
+        server_params_dict = {}
+        for server_name, config in servers.items():
+            server_params = StdioServerParameters(
+                command=config["command"], args=config["args"], env=None
+            )
 
-    server_params2 = StdioServerParameters(
-        command=config1["command"], args=config1["args"], env=None
-    )
+            server_params_dict[server_name] = server_params
 
     stack = AsyncExitStack()
     async with stack:
-        stdio, write = await stack.enter_async_context(stdio_client(server_params))
-        session = await stack.enter_async_context(ClientSession(stdio, write))
-        await session.initialize()
+        session_dict = {}
+        tool_info_dict = {}
+        for server_name, server_params in server_params_dict.items():
+            stdio, write = await stack.enter_async_context(stdio_client(server_params))
+            session = await stack.enter_async_context(ClientSession(stdio, write))
+            await session.initialize()
+            session_dict[server_name] = session
 
-        stdio2, write2 = await stack.enter_async_context(stdio_client(server_params2))
-        session2 = await stack.enter_async_context(ClientSession(stdio2, write2))
-        await session2.initialize()
+            tools_response = await session.list_tools()
+            print(
+                f"Server Name: {server_name}, Available tools: {[tool.name for tool in tools_response.tools]}"
+            )
 
-        tools_response = await session.list_tools()
-        print(f"Available tools: {[tool.name for tool in tools_response.tools]}")
-
-        tool_response2 = await session2.list_tools()
-        print(f"Available tools1: {[tool.name for tool in tool_response2.tools]}")
-
-        for tool in tools_response.tools:
-            MCP_TOOLS[tool.name] = {
-                "description": tool.description,
-                "input_schema": tool.inputSchema,
+            tool_info = {
+                tool.name: {
+                    "description": tool.description,
+                    "input_schema": tool.inputSchema,
+                }
+                for tool in tools_response.tools
             }
-
-        for tool in tool_response2.tools:
-            print(tool.name, tool.description, tool.inputSchema)
+            tool_info_dict[server_name] = tool_info
 
         ollama_client = Client(host="http://localhost:11434")
 
@@ -155,14 +159,26 @@ async def main():
             if user_input.strip().lower() in {"exit", "quit"}:
                 break
 
-            tool_call = choose_mcp_tool(ollama_client, user_input, user_context_list)
-            if tool_call["tool"] not in list(MCP_TOOLS.keys()):
-                print(f"Invalid tool call: {tool_call}")
-                continue
-            else:
-                print(f"Calling tool call: {tool_call}")
+            tool_call = choose_mcp_tool(
+                ollama_client, user_input, user_context_list, tool_info_dict
+            )
 
-            result = await session.call_tool(tool_call["tool"], tool_call["args"])
+            choosen_server_name = tool_call["server_name"]
+
+            if choosen_server_name not in list(tool_info_dict.keys()):
+                print(f"Invalid server call: {tool_call}")
+                continue
+
+            choosen_tool_name = tool_call["tool"]
+
+            if choosen_tool_name not in tool_info_dict[choosen_server_name].keys():
+                print(f"Invalid tool call: {tool_call}")
+
+            print(f"Calling tool call: {tool_call}")
+
+            result = await session_dict[choosen_server_name].call_tool(
+                tool_call["tool"], tool_call["args"]
+            )
             print(result.content[0].text)
 
             user_context_list.append(
