7e3e8f6977e2a09e912e26c44eb7038a25c7a11b
Yunhong Min <yunhongmin00@gmail.com>
2025-07-21 23:09:47 +0900

    chromadb poc

--- client.py
+++ client.py
@@ -3,25 +3,56 @@ import json
 import logging
 from contextlib import AsyncExitStack
 
+import chromadb
 from mcp import StdioServerParameters, stdio_client, ClientSession
 from ollama import Client
 
 logger = logging.getLogger(__name__)
 
 
-def choose_mcp_tool(client, user_query, user_context_list, mcp_tools_dict):
+def convert_to_tool_txt_data(server_name, tool_info_dict):
     tool_description_lines = []
-    for server_name, tool_info_dict in mcp_tools_dict.items():
-        server_name = f"- server_name: {server_name}"
-        tool_description_lines.append(server_name)
+    server_name = f"- server_name: {server_name}"
+    tool_description_lines.append(server_name)
 
-        for tool_name, tool_info in tool_info_dict.items():
-            tool_name_text = f"    - tool_name: {tool_name}"
-            tool_description_lines.append(tool_name_text)
-            tool_info_text = f"    - Tool Info: {tool_info}"
-            tool_description_lines.append(tool_info_text)
+    for tool_name, tool_info in tool_info_dict.items():
+        tool_name_text = f"    - tool_name: {tool_name}"
+        tool_description_lines.append(tool_name_text)
+        tool_info_text = f"    - Tool Info: {tool_info}"
+        tool_description_lines.append(tool_info_text)
 
     tool_description = "\n".join(tool_description_lines)
+    return tool_description
+
+
+def convert_to_tool_data(server_name, tool_name, tool_info):
+    tool_description_lines = []
+    server_name = f"- server_name: {server_name}"
+    tool_description_lines.append(server_name)
+    tool_name_text = f"    - tool name: {tool_name}"
+    tool_description_lines.append(tool_name_text)
+    tool_info_text = f"    - tool info: {tool_info}"
+    tool_description_lines.append(tool_info_text)
+
+    tool_description = "\n".join(tool_description_lines)
+    return tool_description
+
+
+def choose_mcp_tool(
+    client,
+    user_query,
+    user_context_list,
+    mcp_tools_dict,
+    server_name,
+    tool_name,
+    document,
+):
+    # tool_description_lines = []
+    # for server_name, tool_info_dict in mcp_tools_dict.items():
+    #     this_tool_description = convert_to_tool_txt_data(server_name, tool_info_dict)
+    #     tool_description_lines.append(this_tool_description)
+    #
+    # tool_description = "\n".join(tool_description_lines)
 
     user_contexts = "\n".join(
         [
@@ -34,8 +65,10 @@ def choose_mcp_tool(client, user_query, user_context_list, mcp_tools_dict):
     You are a tool-choosing assistant. A user wants to interact with a file system.
     You have access to the following MCP tools:
 
+    Selected server_name: {server_name}
+    Selected tool_name: {tool_name}
     Tool Description:
-    {tool_description}
+    {document}
 
     User query:
     "{user_query}"
@@ -72,6 +105,8 @@ def choose_mcp_tool(client, user_query, user_context_list, mcp_tools_dict):
 
 
 async def main():
+    chromadb_client = chromadb.Client()
+    tool_collection = chromadb_client.get_or_create_collection("tools")
     with open("mcp_server_config.json") as f:
         servers = json.load(f)["mcpServers"]
 
@@ -86,7 +121,7 @@ async def main():
     stack = AsyncExitStack()
     async with stack:
         session_dict = {}
-        tool_info_dict = {}
+        server_name_tool_info_dict = {}
         for server_name, server_params in server_params_dict.items():
             stdio, write = await stack.enter_async_context(stdio_client(server_params))
             session = await stack.enter_async_context(ClientSession(stdio, write))
@@ -105,7 +140,23 @@ async def main():
                 }
                 for tool in tools_response.tools
             }
-            tool_info_dict[server_name] = tool_info
+            server_name_tool_info_dict[server_name] = tool_info
+
+        # initialize chromadb tool
+        ids = []
+        documents = []
+        for server_name, tool_info_dict in server_name_tool_info_dict.items():
+            for tool_name, tool_info in tool_info_dict.items():
+                id_ = f"{server_name}:{tool_name}"
+                ids.append(id_)
+
+                document = convert_to_tool_data(server_name, tool_name, tool_info)
+                documents.append(document)
+
+        tool_collection.add(
+            documents=documents,
+            ids=ids,
+        )
 
         ollama_client = Client(host="http://localhost:11434")
 
@@ -116,8 +167,22 @@ async def main():
             if user_input.strip().lower() in {"exit", "quit"}:
                 break
 
+            tool_result = tool_collection.query(
+                query_texts=[user_input],
+                n_results=1,
+            )
+
+            id_ = tool_result["ids"][0][0]
+            server_name, tool_name = id_.split(":")
+            document = tool_result["documents"][0][0]
             tool_call = choose_mcp_tool(
-                ollama_client, user_input, user_context_list, tool_info_dict
+                ollama_client,
+                user_input,
+                user_context_list,
+                server_name_tool_info_dict,
+                server_name,
+                tool_name,
+                document,
             )
 
             if tool_call is None:
@@ -125,13 +190,16 @@ async def main():
 
             chosen_server_name = tool_call["server_name"]
 
-            if chosen_server_name not in list(tool_info_dict.keys()):
+            if chosen_server_name not in list(server_name_tool_info_dict.keys()):
                 print(f"Invalid server call: {tool_call}")
                 continue
 
             chosen_tool_name = tool_call["tool_name"]
 
-            if chosen_tool_name not in tool_info_dict[chosen_server_name].keys():
+            if (
+                chosen_tool_name
+                not in server_name_tool_info_dict[chosen_server_name].keys()
+            ):
                 print(f"Invalid tool call: {tool_call}")
 
             print(f"Calling tool call: {tool_call}")
