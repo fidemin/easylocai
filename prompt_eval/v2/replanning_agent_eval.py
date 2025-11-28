6421b79d278b8de5fffe795a98e7f842ffb95629
Yunhong Min <yunhongmin00@gmail.com>
2025-11-28 16:03:40 +0900

    single task eval

--- prompt_eval/v2/replanning_agent_eval.py
+++ prompt_eval/v2/replanning_agent_eval.py
@@ -1,62 +1,10 @@
 import asyncio
-import json
 
 from ollama import AsyncClient
 
 from prompt_eval.v2.eval_workflow import EvalWorkflow
 from src.agents.evals.replan_eval_agent import ReplanEvalAgent
 from src.agents.replan_agent import ReplanAgent
-
-
-# def print_evals(
-#     original_user_query,
-#     original_plan,
-#     step_results,
-#     response,
-#     eval,
-# ):
-#     print("-------------------------")
-#     print(f"User Query: {original_user_query}")
-#     print(f"Steps: {original_plan}")
-#     print(f"Step Results: {step_results}")
-#     print(f"Response: {response}")
-#     print(f"Eval: {eval}")
-#     print()
-#
-#
-# async def process(agent, replan_eval_agent, input_dict):
-#     response = await agent.run(**input_dict)
-#     eval = await replan_eval_agent.run(**input_dict, **response)
-#     print_evals(
-#         input_dict["original_user_query"],
-#         input_dict["original_plan"],
-#         input_dict["step_results"],
-#         response,
-#         eval,
-#     )
-#
-#
-# async def main():
-#     with open("./resources/prompt_eval/replan_agent_inputs.json") as f:
-#         input_dicts = json.load(f)
-#
-#     ollama_client = AsyncClient(host="http://localhost:11434")
-#     replan_agent = ReplanAgent(client=ollama_client)
-#     replan_eval_agent = ReplanEvalAgent(client=ollama_client)
-#
-#     tasks = []
-#     for input_dict in input_dicts:
-#         task = asyncio.create_task(process(replan_agent, replan_eval_agent, input_dict))
-#         tasks.append(task)
-#
-#     results = await asyncio.gather(*tasks, return_exceptions=True)
-#
-#     for i, result in enumerate(results):
-#         if isinstance(result, Exception):
-#             print(f"Task {i} failed for input_dict:")
-#             print(input_dicts[i])
-#             print("Exception:", repr(result))
-
 
 if __name__ == "__main__":
     ollama_client = AsyncClient(host="http://localhost:11434")
--- prompt_eval/v2/single_task_agent_eval.py
+++ prompt_eval/v2/single_task_agent_eval.py
@@ -1,63 +1,22 @@
 import asyncio
-import json
 import logging.config
 
 from ollama import AsyncClient
 
+from prompt_eval.v2.eval_workflow import EvalWorkflow
 from src.agents.single_task_agent import SingleTaskAgent
 from src.utlis.loggers.default_dict import default_logging_config
 
 logging.config.dictConfig(default_logging_config)
 
 
-def print_evals(
-    input_dict,
-    response,
-    eval,
-):
-    print("-------------------------")
-    print(f"Input Dict:")
-    print(json.dumps(input_dict, indent=2))
-    print(f"Response:")
-    print(json.dumps(response, indent=2))
-    print(f"Eval:\n{eval}")
-    print()
-
-
-async def process(agent, replan_eval_agent, input_dict):
-    response = await agent.run(**input_dict)
-    if replan_eval_agent is not None:
-        eval = await replan_eval_agent.run(**input_dict, **response)
-    else:
-        eval = None
-    print_evals(
-        input_dict,
-        response,
-        eval,
-    )
-
-
-async def main():
-    with open("./resources/prompt_eval/single_task_agent_inputs.json") as f:
-        input_dicts = json.load(f)
-
-    ollama_client = AsyncClient(host="http://localhost:11434")
-    replan_agent = SingleTaskAgent(client=ollama_client)
-    # replan_eval_agent = ReplanEvalAgent(client=ollama_client)
-
-    tasks = []
-    for input_dict in input_dicts:
-        task = asyncio.create_task(process(replan_agent, None, input_dict))
-        tasks.append(task)
-
-    results = await asyncio.gather(*tasks, return_exceptions=True)
-
-    for i, result in enumerate(results):
-        if isinstance(result, Exception):
-            print(f"Task {i} failed for input_dict:")
-            print(input_dicts[i])
-            print("Exception:", repr(result))
-
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    ollama_client = AsyncClient(host="http://localhost:11434")
+    plan_agent = SingleTaskAgent(client=ollama_client)
+    plan_eval_agent = None
+    workflow = EvalWorkflow(
+        agent=plan_agent,
+        eval_agent=plan_eval_agent,
+        input_path="./resources/prompt_eval/single_task_agent_inputs.json",
+    )
+    asyncio.run(workflow.run())
