aee33fcc94a4f8e74f9feca93085089ae59d23d5
Yunhong Min <yunhongmin00@gmail.com>
2025-12-06 22:52:30 +0900

    Task Agent added

--- run_beta.py
+++ run_beta.py
@@ -9,6 +9,7 @@ from rich import get_console
 from src.agents.plan_agent import PlanAgent
 from src.agents.replan_agent import ReplanAgent
 from src.agents.single_task_agent import SingleTaskAgent
+from src.agents.task_agent import TaskAgent
 from src.core.server import ServerManager
 from src.utlis.console_util import multiline_input, render_chat, ConsoleSpinner
 from src.utlis.loggers.default_dict import default_logging_config
@@ -57,6 +58,11 @@ async def main():
     plan_agent = PlanAgent(client=ollama_client)
     replan_agent = ReplanAgent(client=ollama_client)
     single_task_agent = SingleTaskAgent(client=ollama_client)
+    task_agent = TaskAgent(
+        client=ollama_client,
+        tool_collection=tool_collection,
+        server_manager=server_manager,
+    )
 
     messages = []
 
@@ -89,87 +95,18 @@ async def main():
                 logger.debug(f"Plan Agent Response:\n{plan_agent_response}")
                 tasks = plan_agent_response["tasks"]
                 previous_task_results = []
-                answer = None
 
                 while True:
                     task = tasks[0]
+                    task_agent_query = {
+                        "original_tasks": tasks,
+                        "original_user_query": user_input,
+                        "task": task,
+                        "previous_task_list": previous_task_results,
+                    }
+                    task_agent_response = await task_agent.run(**task_agent_query)
 
-                    iteration_results = []
-
-                    while True:
-                        spinner.set_prefix(f"Executing task: {task}...")
-
-                        tool_search_result = tool_collection.query(
-                            query_texts=[task],
-                            n_results=5,
-                        )
-
-                        metadatas = tool_search_result["metadatas"][0]
-                        tool_candidates = []
-
-                        for metadata in metadatas:
-                            server_name = metadata["server_name"]
-                            tool_name = metadata["tool_name"]
-                            tool = server_manager.get_server(server_name).get_tool(
-                                tool_name
-                            )
-                            tool_candidates.append(
-                                {
-                                    "server_name": server_name,
-                                    "tool_name": tool.name,
-                                    "tool_description": tool.description,
-                                    "tool_input_schema": tool.input_schema,
-                                }
-                            )
-
-                        single_task_agent_query = {
-                            "original_tasks": tasks,
-                            "original_user_query": user_input,
-                            "task": task,
-                            "previous_task_list": previous_task_results,
-                            "iteration_results": iteration_results,
-                            "tool_candidates": tool_candidates,
-                        }
-
-                        single_task_agent_response = await single_task_agent.run(
-                            **single_task_agent_query
-                        )
-                        logger.debug(
-                            f"Single Task Agent Response:\n{single_task_agent_response}"
-                        )
-                        finished = single_task_agent_response["finished"]
-
-                        if finished:
-                            break
-
-                        if single_task_agent_response["use_llm"] is True:
-                            # TODO: implement LLM reasoning here
-                            break
-
-                        tool_result = await server_manager.call_tool(
-                            single_task_agent_response["server_name"],
-                            single_task_agent_response["tool_name"],
-                            single_task_agent_response.get("tool_args"),
-                        )
-
-                        logger.debug(f"original tool result:\n{tool_result}")
-                        iteration_results.append(
-                            {
-                                "executed_task": single_task_agent_response[
-                                    "executed_task"
-                                ],
-                                "result": tool_result,
-                            }
-                        )
-
-                    # TODO: task / step terminology unification
-                    previous_task_results.append(
-                        {
-                            "task": task,
-                            "step": task,
-                            "result": iteration_results,
-                        }
-                    )
+                    previous_task_results.append(task_agent_response)
 
                     replan_agent_query = {
                         "original_user_query": user_input,
@@ -181,6 +118,7 @@ async def main():
                     logger.debug(f"Replan Agent Response:\n{replan_agent_response}")
 
                     response = replan_agent_response["response"]
+
                     if response is not None:
                         answer = response
                         break
---
+++ src/agents/task_agent.py
@@ -0,0 +1,135 @@
+import json
+import logging
+
+from chromadb.types import Collection
+from jinja2 import Environment, FileSystemLoader
+from ollama import AsyncClient
+
+from src.core.agent import Agent
+from src.core.contants import DEFAULT_LLM_MODEL
+from src.core.server import ServerManager
+from src.utlis.prompt import pretty_prompt_text
+
+logger = logging.getLogger(__name__)
+
+
+class TaskAgent(Agent):
+    _prompt_path = "resources/prompts/v2/single_task_prompt_v2.jinja2"
+
+    def __init__(
+        self,
+        *,
+        client: AsyncClient,
+        tool_collection: Collection,
+        server_manager: ServerManager,
+    ):
+        self._ollama_client = client
+        env = Environment(loader=FileSystemLoader(""))
+        prompt_template = env.get_template(self._prompt_path)
+        self._prompt_template = prompt_template
+        self._model = DEFAULT_LLM_MODEL
+        self._tool_collection = tool_collection
+        self._server_manager = server_manager
+
+    async def run(self, **query) -> str | dict:
+        task = query["task"]
+
+        tool_search_result = self._tool_collection.query(
+            query_texts=[task],
+            n_results=5,
+        )
+
+        metadatas = tool_search_result["metadatas"][0]
+        tool_candidates = []
+
+        for metadata in metadatas:
+            server_name = metadata["server_name"]
+            tool_name = metadata["tool_name"]
+            tool = self._server_manager.get_server(server_name).get_tool(tool_name)
+            tool_candidates.append(
+                {
+                    "server_name": server_name,
+                    "tool_name": tool.name,
+                    "tool_description": tool.description,
+                    "tool_input_schema": tool.input_schema,
+                }
+            )
+
+        previous_task_results = query.get("previous_task_results", [])
+        original_tasks = query.get("original_tasks", None)
+        iteration_results = []
+
+        if original_tasks is None:
+            logger.warning(
+                "original_tasks is not provided to SingleTaskAgent, defaulting to [task]"
+            )
+            original_tasks = []
+
+        while True:
+            prompt = self._prompt_template.render(
+                tool_candidates=tool_candidates,
+                previous_task_results=previous_task_results,
+                iteration_results=iteration_results,
+                original_tasks=original_tasks,
+                task=task,
+            )
+            logger.debug(pretty_prompt_text("Task Prompt", prompt))
+
+            response = await self._ollama_client.chat(
+                model=self._model,
+                messages=[
+                    {"role": "system", "content": prompt},
+                ],
+                options={
+                    "temperature": 0.2,
+                },
+            )
+
+            try:
+                task_result = json.loads(response["message"]["content"])
+            except json.JSONDecodeError:
+                logger.error(
+                    f"Failed to parse SingleTaskAgent response as JSON: {response['message']['content']}"
+                    + f"\nThinking: {response['message']['thinking']}"
+                )
+                iteration_results.append(
+                    {
+                        "executed_task": task,
+                        "result": f"Error to parse result to JSON."
+                        f"\nContent:{response['message']['content']}"
+                        f"\nThinking: {response['message']['thinking']}",
+                    }
+                )
+                continue
+
+            logger.debug(f"Task Prompt Response:\n{task_result}")
+            finished = task_result["finished"]
+
+            if finished:
+                break
+
+            if task_result["use_llm"] is True:
+                # TODO: implement LLM reasoning here
+                return {
+                    "task": task,
+                    "result": "LLM reasoning not implemented. This task requires LLM reasoning.",
+                }
+
+            tool_result = await self._server_manager.call_tool(
+                task_result["server_name"],
+                task_result["tool_name"],
+                task_result["tool_args"],
+            )
+
+            logger.debug(f"Tool result:\n{tool_result}")
+            iteration_results.append(
+                {
+                    "executed_task": task_result["executed_task"],
+                    "result": tool_result,
+                }
+            )
+
+        return {
+            "task": task,
+            "result": iteration_results,
+        }
