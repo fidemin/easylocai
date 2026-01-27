435136b6247e9302b8959161af259264f0e325d8
Yunhong Min <yunhongmin00@gmail.com>
2026-01-27 14:42:46 +0900

    refact: remove unused files and remaming

--- prompt_eval/v2/replan_prompt_eval_v2.py
+++ prompt_eval/v2/replan_prompt_eval_v2.py
@@ -4,10 +4,10 @@ from easylocai.llm_calls.replanner import ReplannerV2Output
 from prompt_eval.prompt_eval_workflow import PromptEvalWorkflow
 
 if __name__ == "__main__":
-    input_file_path = "resources/prompt_eval/replan_prompt_inputs_v2.json"
+    input_file_path = "resources/prompt_eval/replan_prompt_inputs.json"
     prompt_info = {
-        "system": "resources/prompts/v2/replanner_system_prompt_v2.jinja2",
-        "user": "resources/prompts/v2/replanner_user_prompt_v2.jinja2",
+        "system": "resources/prompts/v2/replanner_system_prompt.jinja2",
+        "user": "resources/prompts/v2/replanner_user_prompt.jinja2",
     }
     model_info = {
         "host": "http://localhost:11434",
--- prompt_eval/v2/tool_select_prompt_eval_v2.py
+++ prompt_eval/v2/tool_select_prompt_eval_v2.py
@@ -4,10 +4,10 @@ from easylocai.llm_calls.tool_selector import ToolSelectorV2Output
 from prompt_eval.prompt_eval_workflow import PromptEvalWorkflow
 
 if __name__ == "__main__":
-    input_file_path = "resources/prompt_eval/tool_selector_prompt_inputs_v2.json"
+    input_file_path = "resources/prompt_eval/tool_selector_prompt_inputs.json"
     prompt_info = {
-        "system": "resources/prompts/v2/tool_selector_system_prompt_v2.jinja2",
-        "user": "resources/prompts/v2/tool_selector_user_prompt_v2.jinja2",
+        "system": "resources/prompts/v2/tool_selector_system_prompt.jinja2",
+        "user": "resources/prompts/v2/tool_selector_user_prompt.jinja2",
     }
     model_info = {
         "host": "http://localhost:11434",
